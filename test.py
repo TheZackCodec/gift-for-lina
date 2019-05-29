00df75a153b7874eb16f391e49ee187b21912246
Derick Vigne <derickvigne@me.com>
2019-05-29 19:51:48 -0400

    Added ASCII Art Assets and Animation Functions

--- test.py
+++ test.py
@@ -1,71 +1,80 @@
+##############################################
+#   Author: Derick Vigne
+#   Todo:
+#   - Replace MaxY, MaxX with functional arguments
+#   - Organize into story pages or phases
+#
+##############################################
+
 import os, sys
 import logging
 import random
 import time
 import curses
 
-star = "✧"
-heart = """
- ____
- |  /
-  |/\\_\\_
-      \\_\\_
-        \\_\\_
-          \\.\\---..__             __..---,._
-       .,'          "-.       .-"          `,.
-     ,'                `.   .'                `,
-    /                   `,_,'                      \\
-   |                                             |
-   ;                                             ;
-    ;                                           ;
-     `,                                       ,'
-      `,                                      ,'
-       `,                                  ,'
-        `,                                ,'
-         `,                              ,'
-           `,                          ,'
-             `,                     \\_\\_
-               `,                   ,'\\_\\_
-                 `,               ,'    \\_\\_
-                   `,           ,'        \\+--
-                     `,       ,'           | +--
-                       `,   ,'               | +--
-                         "-"
-"""
+heart = open('heart.txt', 'r').read()
+birthday = open('birthday.txt', 'r').read()
 
-def drawScreen(stdscr):
-    maxY = stdscr.getmaxyx()[0]
-    maxX = stdscr.getmaxyx()[1]
-    logging.debug("Max Y, X: %s, %s" % (maxY, maxX))
-    while(True):
-        stdscr.erase()
-        for ittr in range(0, maxX):
-            stdscr.addch(random.randrange(maxY), random.randrange(maxX), star)
-            time.sleep(.1)
-            stdscr.refresh()
+starArray = []
 
-def drawHeart(stdscr):
-    maxY = stdscr.getmaxyx()[0]
-    maxX = stdscr.getmaxyx()[1]
+def drawHappyBirthday(stdscr, maxY, maxX):
     logging.debug("Max Y, X: %s, %s" % (maxY, maxX))
-    stdscr.erase()
+    stdscr.addstr(birthday)
+
+def drawStars(stdscr, maxY, maxX):
+    global starArray
+    logging.debug("Max Y, X: %s, %s" % (maxY, maxX))
+    for ittr in range(0, 100):
+        randomY = random.randrange(maxY)
+        randomX = random.randrange(maxX)
+        starArray.append([randomY, randomX])
+        stdscr.addch(randomY, randomX, curses.ACS_DIAMOND, curses.A_DIM)
+    logging.debug("Array: %s", starArray)
+
+def twinkleRandomStar(stdscr):
+    global starArray
+    for x in range(0, int(len(starArray) / 2)):
+        randCoordinates = starArray[random.randrange(len(starArray))]
+        if(random.random() > .5):
+            stdscr.addch(randCoordinates[0], randCoordinates[1], curses.ACS_DIAMOND, curses.A_BOLD)
+        else:
+            stdscr.addch(randCoordinates[0], randCoordinates[1], curses.ACS_DIAMOND, curses.A_DIM)
+
+def drawHeart(stdscr, maxY, maxX):
+    logging.debug("Max Y, X: %s, %s" % (maxY, maxX))
     stdscr.addstr(heart)
-    while(True):
-        stdscr.refresh()
+
 def main():
     random.seed(time.time())
     logging.basicConfig(filename="test.log", level=logging.DEBUG, format='%(asctime)s %(message)s')
 
     stdscr = curses.initscr()
+    curses.start_color()
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
 
+    maxY = stdscr.getmaxyx()[0];
+    maxX = stdscr.getmaxyx()[1];
+
+    heartWin = curses.newwin(38, 83, int((maxY / 2) - (38 / 2) + 4), int((maxX / 2) - (83 / 2)))
+    birthdayWin = curses.newwin(7, 129, 0, int((maxX / 2) - (129/2)))
     try:
-        drawScreen(stdscr)
+        drawStars(stdscr, maxY, maxX)
+        while(True):
+            drawHeart(heartWin, maxY, maxX)
+            drawHappyBirthday(birthdayWin, maxY, maxX)
+            stdscr.refresh()
+            birthdayWin.refresh()
+            heartWin.refresh()
+            twinkleRandomStar(stdscr)
+            time.sleep(.15)
+            birthdayWin.erase()
+            heartWin.erase()
     except KeyboardInterrupt:
         curses.curs_set(1)
         curses.nocbreak()
+        curses.echo()
         curses.endwin()
 
 
