07dc324152da74824ab34192867cdadb211134f8
Derick Vigne <derickvigne@me.com>
2019-05-31 22:13:20 -0400

    Added Slide 1 Photo Function

--- gfl.py
+++ gfl.py
@@ -11,13 +11,16 @@ import logging
 import random
 import time
 import curses
+from PIL import Image
 
 heart = open('ascii/heart.txt', 'r').read()
 birthday = open('ascii/birthday.txt', 'r').read()
 
 starArray = []
+maxY = None
+maxX = None
 
-def drawHappyBirthday(stdscr, maxY, maxX):
+def drawHappyBirthday(stdscr):
     stdscr.addstr(birthday)
 
 def drawStars(stdscr, maxY, maxX):
@@ -27,7 +30,7 @@ def drawStars(stdscr, maxY, maxX):
         randomX = random.randrange(maxX)
         starArray.append([randomY, randomX])
         stdscr.addch(randomY, randomX, curses.ACS_DIAMOND, curses.A_DIM)
-    logging.debug("Array: %s", starArray)
+    logging.debug("Star Array: %s", starArray)
 
 def twinkleRandomStar(stdscr):
     global starArray
@@ -38,12 +41,48 @@ def twinkleRandomStar(stdscr):
         else:
             stdscr.addch(randCoordinates[0], randCoordinates[1], curses.ACS_DIAMOND, curses.A_DIM)
 
-def drawHeart(stdscr, maxY, maxX):
+def drawHeart(stdscr):
     stdscr.addstr(heart)
 
+def endSession():
+    logging.debug('Ending Session')
+    curses.curs_set(1)
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
+
+def scene1(stdscr):
+    heartWin = curses.newwin(38, 83, int((maxY / 2) - (38 / 2) + 4), int((maxX / 2) - (83 / 2)))
+    birthdayWin = curses.newwin(7, 129, 0, int((maxX / 2) - (129/2)))
+
+    drawStars(stdscr, maxY, maxX)
+    epoch = time.time() + 5
+    while(time.time() < epoch):
+        drawHeart(heartWin)
+        drawHappyBirthday(birthdayWin)
+        stdscr.refresh()
+        birthdayWin.refresh()
+        heartWin.refresh()
+        twinkleRandomStar(stdscr)
+        time.sleep(.15)
+        birthdayWin.erase()
+        heartWin.erase()
+
+def slideShow1():
+    for x in range(1, 20):
+        img = Image.open('img/%s.jpg' % x)
+        logging.debug('Image %s loaded with dimensions %s x %s' % (img.filename, img.size[0], img.size[1]))
+        img = img.resize([int(img.size[0] / 2), int(img.size[1] / 2)], Image.BICUBIC)
+        logging.debug('Image resized to %s x %s' % (img.size[0], img.size[1]))
+        img.show()
+        time.sleep(1)
+    os.system('dialog --msgbox "Continue..." 10 50')
+    os.system('pkill display')
+
 def main():
+    global maxY, maxX
     random.seed(time.time())
-    logging.basicConfig(filename="gfl.log", level=logging.DEBUG, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename="gfl.log", level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
     stdscr = curses.initscr()
     curses.start_color()
@@ -51,31 +90,23 @@ def main():
     curses.cbreak()
     curses.curs_set(0)
 
-    maxY = stdscr.getmaxyx()[0];
-    maxX = stdscr.getmaxyx()[1];
+    maxY = stdscr.getmaxyx()[0]
+    maxX = stdscr.getmaxyx()[1]
 
     logging.debug("Max Y, X: %s, %s" % (maxY, maxX))
-    
-    heartWin = curses.newwin(38, 83, int((maxY / 2) - (38 / 2) + 4), int((maxX / 2) - (83 / 2)))
-    birthdayWin = curses.newwin(7, 129, 0, int((maxX / 2) - (129/2)))
-    try:
-        drawStars(stdscr, maxY, maxX)
-        while(True):
-            drawHeart(heartWin, maxY, maxX)
-            drawHappyBirthday(birthdayWin, maxY, maxX)
-            stdscr.refresh()
-            birthdayWin.refresh()
-            heartWin.refresh()
-            twinkleRandomStar(stdscr)
-            time.sleep(.15)
-            birthdayWin.erase()
-            heartWin.erase()
-    except KeyboardInterrupt:
-        curses.curs_set(1)
-        curses.nocbreak()
-        curses.echo()
-        curses.endwin()
 
+
+    try:
+        scene1(stdscr)
+        slideShow1()
+        endSession()
+
+    except KeyboardInterrupt:
+        logging.warn("Keyboard Interrupt Event Detected...shutting down")
+        endSession()
+    except:
+        logging.error("%s" % sys.exc_info()[0])
+        endSession()
 
 if __name__ == "__main__":
     main()
