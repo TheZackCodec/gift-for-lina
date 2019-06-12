38496d7e5c6c8b3249b52dc5980d650bd41d33ba
Derick Vigne <derickvigne@me.com>
2019-06-12 00:08:45 -0400

    Fixed Paths

--- gfl.sh
+++ gfl.sh
@@ -3,9 +3,9 @@
 brew install git python dialog;
 git clone https://github.com/dvigne/gift-for-lina.git /tmp;
 cd /tmp/gift-for-lina;
-pip install virtualenv;
+pip3 install virtualenv;
 virtualenv --system-site-packages env && source ./env/bin/activate;
-pip install -r ./requirements.txt
-dialog --yesno "Is your volume up?" 10 50 && dialog --yesno "Is your terminal maximized, not fullscreened?" 10 50 && python gfl.py;
+pip3 install -r ./requirements.txt
+dialog --yesno "Is your volume up?" 10 50 && dialog --yesno "Is your terminal maximized, not fullscreened?" 10 50 && python3 gfl.py;
 deactivate;
 rm -R -v -f /tmp/gift-for-lina;
