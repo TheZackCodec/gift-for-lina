24eb86d7f9c7943250cd03acf775417b1beb2541
Derick Vigne <derickvigne@me.com>
2019-06-19 17:01:18 -0400

    Added Proper Paths to Linux Script -- Added libgstreamer Dependency"

--- gfl-linux.sh
+++ gfl-linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-apt install git python3 python3-pip dialog;
-git clone https://github.com/dvigne/gift-for-lina.git /tmp;
+apt update; apt install -y git python3 python3-pip dialog libgstreamer1.0-0;
+git clone https://github.com/dvigne/gift-for-lina.git /tmp/gift-for-lina;
 cd /tmp/gift-for-lina;
-pip install virtualenv;
+pip3 install virtualenv;
 virtualenv --system-site-packages  -p python3 env && source ./env/bin/activate;
-pip3 install -r ./requirements.txt
-dialog --yesno "Is your volume up?" 10 50 && dialog --yesno "Is your terminal maximized, not fullscreened?" 10 50 && python3 gfl.py;
+pip3 install -r ./requirements_linux.txt
+dialog --yesno "Is your volume up?" 10 50 && dialog --yesno "Is your terminal maximized, not fullscreened?" 10 50 && python3 ./gfl.py;
 deactivate;
 rm -R -v -f /tmp/gift-for-lina;
--- gfl.sh
+++ gfl.sh
@@ -5,7 +5,7 @@ git clone https://github.com/dvigne/gift-for-lina.git /tmp/gift-for-lina;
 cd /tmp/gift-for-lina;
 pip3 install virtualenv;
 virtualenv --system-site-packages env && source ./env/bin/activate;
-pip3 install -r ./requirements.txt
+pip3 install -r ./requirements_mac.txt
 dialog --yesno "Is your volume up?" 10 50 && dialog --yesno "Is your terminal maximized, not fullscreened?" 10 50 && python3 ./gfl.py;
 deactivate;
 rm -R -v -f /tmp/gift-for-lina;
